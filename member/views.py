14cb398de02da1862b293d43ca389b5a3fb79c3c
parkgyoingin <gghotted@naver.com>
2019-06-27 01:58:00 +0900

    0627home

--- member/views.py
+++ member/views.py
@@ -25,4 +25,27 @@ def tryJoin(request):
 # Create your views here.
 
 def idCheck(request):
-    return render(request, "member/idCheck.html")
+    id = request.POST['user_id']
+    is_exist =  len(Member.objects.filter(user_id=id))
+    can_use = canUse(id)
+    check = False
+    if len(id) == 0:
+        result = "아이디를 입력해주세요"
+    elif is_exist:
+        result = ' "{}" 는 이미 사용중인 아이디 입니다'.format(id)
+    elif not can_use:
+        result = "아이디는 영문(소문자), 숫자로만 구성되어야 합니다"
+    else:
+        result = "'{}' 는 사용 가능합니다".format(id)
+        check = True
+
+    print("id : %s, %s"%(id,result))
+    context = {"check":check, "result":result}
+    return render(request, "member/idCheck.html", context)
+
+def canUse(id):
+    for c in id:
+        if not c.islower() and not c.isdecimal():
+            return False
+    return True
+
