596d4f840c63037842c000065dd4d544a36cd62b
parkgyoingin <gghotted@naver.com>
2019-07-01 01:38:05 +0900

    0701.home

--- member/urls.py
+++ member/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index, name='join'),
     path('tryJoin/',views.tryJoin, name='tryJoin'),
     path('idCheck/',views.idCheck, name='idCheck'),
+    path('login/', views.login, name='login'),
+    path("completeJoin/", views.test, name='test'),
 ]
--- member/views.py
+++ member/views.py
@@ -6,29 +6,29 @@ def index(request):
     return render(request,'member/index.html')
 
 def tryJoin(request):
-    #print(request.POST)
     #member = Member.objects.all() #member[row].colum
 
-    #member = Member(name=request.POST['name'], user_id=request.POST['user_id'], password=request.POST['password']
-    #                , phone_number=request.POST['phone_number'] , gender=request.POST['gender'] , animal_count=request.POST['animal_count'])
-    #member.save()
-    #member = Member.objects.filter(name='박경민')
     idCheck = len(Member.objects.filter(user_id=request.POST['user_id']))
+    for key in request.POST:
+        print("%s : %s"%(key, request.POST[key]))
     if idCheck:
         print("저장불가")
     else:
         print("저장가능")
+        member = Member(name=request.POST['name'], user_id=request.POST['user_id'], password=request.POST['password']
+                        , phone_number=request.POST['phone_number'] , gender=request.POST['gender'] , animal_count=request.POST['animal_count'])
+        member.save()
+    content = {"user_id":request.POST["user_id"], "name":request.POST["name"]}
+    return render(request,"member/completeJoin.html",content)
 
-
-
-    return HttpResponse("oo")
-# Create your views here.
 
 def idCheck(request):
-    id = request.POST['user_id']
+    id = ""
+    if request.POST.get("user_id"):
+        id = request.POST['user_id']
     is_exist =  len(Member.objects.filter(user_id=id))
     can_use = canUse(id)
-    check = False
+    check = 0
     if len(id) == 0:
         result = "아이디를 입력해주세요"
     elif is_exist:
@@ -37,15 +37,21 @@ def idCheck(request):
         result = "아이디는 영문(소문자), 숫자로만 구성되어야 합니다"
     else:
         result = "'{}' 는 사용 가능합니다".format(id)
-        check = True
+        check = 1
 
-    print("id : %s, %s"%(id,result))
-    context = {"check":check, "result":result}
+    context = {"check":check, "result":result, "user_id":id}
     return render(request, "member/idCheck.html", context)
+
+def login(requset):
+    return render(requset, "member/login.html")
+
+def test(request):
+    return render(request,"member/completeJoin.html")
 
 def canUse(id):
     for c in id:
         if not c.islower() and not c.isdecimal():
             return False
     return True
+
 
